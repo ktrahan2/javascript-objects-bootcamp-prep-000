b90c14fe9e4c3e5eb8f8e4f0f19afc17c8c2cee3
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-11 13:01:21 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,3 +4,7 @@ function updatePlaylist(object, key, value) {
   object[key] = value
   return object
 }
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+  return playlist
+}
